fd91686b5c1ee6fe199390f8eddf3b7ca02ec027
Md Imran Hossain <soil_rana@yahoo.com>
2017-12-06 15:00:55 +0100

    Update CrossingCars.java

--- src/CrossingCars.java
+++ src/CrossingCars.java
@@ -53,7 +53,6 @@ expected worst-case time complexity is O(N);
 expected worst-case space complexity is O(1), beyond input storage (not counting 
 the storage required for input arguments).
  */
-package lesson;
 
 /**
  *
