08980dfd631f5fda0c1985bb24bfab05d5bfc977
Md Imran Hossain <soil_rana@yahoo.com>
2017-12-06 13:33:24 +0100

    Update ArrayMaxSliceSum.java

--- src/ArrayMaxSliceSum.java
+++ src/ArrayMaxSliceSum.java
@@ -38,8 +38,6 @@ Complexity:
         expected worst-case time complexity is O(N);
         expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
  */
-package lesson;
-
 /**
  *
  * @author Hossain
